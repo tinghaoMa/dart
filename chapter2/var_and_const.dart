d0fa91fc3a584365347a66d982918cc00bab5bd0
tinghaoma <tinghaoma@sohu-inc.com>
2018-09-11 23:19:35 +0800

    变量和常量

---
+++ chapter2/var_and_const.dart
@@ -0,0 +1,30 @@
+/**
+ *@BelongsProject: dart
+ *@Author: itck_mth
+ *@CreateTime: 2018-09-11 23:10
+ * 变量和常量
+ */
+
+void main(){
+    //变量默认初始化为 null
+    var a ;
+    print(a);
+
+    a=10;
+    print(a);
+
+    a='hello dart';
+    print(a);
+
+    var b =100;
+    print(b);
+
+    //声明只能赋值一次的值
+    final c= 20;
+//    c=100;
+
+    //常量
+    const d =50;
+//    d=20;
+
+}
