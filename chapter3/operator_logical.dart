3c814a23822b35c0692f44e721cd89504267ce20
tinghaoma <tinghaoma@sohu-inc.com>
2018-09-12 23:40:41 +0800

    逻辑运算符

---
+++ chapter3/operator_logical.dart
@@ -0,0 +1,16 @@
+/**
+ *@BelongsProject: dart
+ *@Author: itck_mth
+ *@CreateTime: 2018-09-12 23:30
+ * 逻辑运算符
+ */
+
+void main() {
+  bool isTrue = false;
+  print(!isTrue);
+
+  bool finished = true;
+  print(isTrue && finished);
+  print(isTrue || finished);
+
+}
