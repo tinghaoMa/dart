911c37bb787f7175c88942dc1765b7ca98b1ad70
tinghaoma <tinghaoma@sohu-inc.com>
2018-09-13 07:38:14 +0800

    while  do while

---
+++ chapter4/stmt_while.dart
@@ -0,0 +1,18 @@
+/**
+ *@BelongsProject: dart
+ *@Author: itck_mth
+ *@CreateTime: 2018-09-13 07:35
+ */
+
+void main() {
+  int count = 0;
+  while (count < 10) {
+    print(count);
+    count++;
+  }
+
+  do {
+    print(count);
+    count--;
+  } while (count >= 0);
+}
