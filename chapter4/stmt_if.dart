6fb0bb0a794dc3687cb62ed24a0d8ed24ef83fa2
tinghaoma <tinghaoma@sohu-inc.com>
2018-09-13 00:11:17 +0800

    if 语句

---
+++ chapter4/stmt_if.dart
@@ -0,0 +1,15 @@
+/**
+ *@BelongsProject: dart
+ *@Author: itck_mth
+ *@CreateTime: 2018-09-13 00:06
+ */
+
+void main() {
+  int score = 68;
+
+  if (score == 100) {
+    print('完美');
+  } else {
+    print(score);
+  }
+}
