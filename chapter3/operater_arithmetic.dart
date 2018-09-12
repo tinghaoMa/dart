4dc29516003a1ce43fd3a7914788085b37478567
tinghaoma <tinghaoma@sohu-inc.com>
2018-09-12 23:24:07 +0800

    操作运算符

---
+++ chapter3/operater_arithmetic.dart
@@ -0,0 +1,27 @@
+/**
+ *@BelongsProject: dart
+ *@Author: itck_mth
+ *@CreateTime: 2018-09-12 23:18
+ * 运算操作符
+ */
+
+void main() {
+  int a = 2;
+  int b = 10;
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a ~/ b);
+  print(a % b);
+
+  print(a++);
+  print(a);
+  print(++a);
+
+  print(a--);
+  print(a);
+  print(--a);
+  
+
+}
