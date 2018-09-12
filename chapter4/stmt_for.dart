4632b91bc5607937f68224173a80c7192912ed5e
tinghaoma <tinghaoma@sohu-inc.com>
2018-09-13 00:16:04 +0800

    for 语句

---
+++ chapter4/stmt_for.dart
@@ -0,0 +1,17 @@
+/**
+ *@BelongsProject: dart
+ *@Author: itck_mth
+ *@CreateTime: 2018-09-13 00:11
+ */
+
+void main() {
+  var list = [1, 2, 3, 4, 5, 6];
+  for (var i = 0; i < list.length; i++) {
+    print(list[i]);
+  }
+
+  for(var item in list){
+    print(item);
+  }
+
+}
